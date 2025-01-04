c2f1ad001c8e2a8c96a0ee2db0516eb137e0d5e5
imaldi <aldiirsanmajid@gmail.com>
2025-01-04 23:13:12 +0700

    completing story list

--- lib/screen/stories_list_screen.dart
+++ lib/screen/stories_list_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:story_app/model/story.dart';
+import 'package:story_app/provider/story_provider.dart';
+import 'package:story_app/static/story_list_result_state.dart';
 
-
-class StoriesListScreen extends StatelessWidget {
+class StoriesListScreen extends StatefulWidget {
   final List<StoryDummy> stories;
   final Function(String) onTapped;
 
@@ -13,21 +15,47 @@ class StoriesListScreen extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<StoriesListScreen> createState() => _StoriesListScreenState();
+}
+
+class _StoriesListScreenState extends State<StoriesListScreen> {
+  @override
+  void initState() {
+    Future.microtask(() {
+      context.read<StoryListProvider>().fetchStoryList();
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Quotes App"),
       ),
-      body: ListView(
-        children: [
-          for (var quote in stories)
-            ListTile(
-              title: Text(quote.author),
-              subtitle: Text(quote.content),
-              isThreeLine: true,
-              onTap: () => onTapped(quote.id),
-            )
-        ],
+      body: Consumer<StoryListProvider>(
+        builder: (context, value, child) {
+          return switch (value.resultListState) {
+            StoryListLoadingState() => const Center(
+                child: CircularProgressIndicator(),
+              ),
+            StoryListLoadedState(data: var storyList) => ListView(
+                children: [
+                  for (var story in storyList)
+                    ListTile(
+                      title: Text(story.name ?? ""),
+                      subtitle: Text(story.description ?? ""),
+                      isThreeLine: true,
+                      onTap: () => widget.onTapped(story.id ?? ""),
+                    )
+                ],
+              ),
+            StoryListErrorState(error: var message) => Center(
+                child: Text(message),
+              ),
+            _ => const SizedBox(),
+          };
+        },
       ),
     );
   }
