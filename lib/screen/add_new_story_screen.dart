cf80f6419c0f245d2e621679eb87436820887380
imaldi <aldiirsanmajid@gmail.com>
2025-01-19 09:14:46 +0700

    done image picker

--- lib/screen/add_new_story_screen.dart
+++ lib/screen/add_new_story_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:story_app/model/story_response.dart';
 import 'package:story_app/provider/add_story_provider.dart';
@@ -21,6 +24,21 @@ class _AddNewStoryScreenState extends State<AddNewStoryScreen> {
 
   final formKey = GlobalKey<FormState>();
 
+  final ImagePicker _picker = ImagePicker();
+  final TextEditingController maxWidthController = TextEditingController();
+  final TextEditingController maxHeightController = TextEditingController();
+  final TextEditingController qualityController = TextEditingController();
+  final TextEditingController limitController = TextEditingController();
+  String? _retrieveDataError;
+
+  List<XFile>? _mediaFileList;
+
+  dynamic _pickImageError;
+
+  void _setImageFileListFromFile(XFile? value) {
+    _mediaFileList = value == null ? null : <XFile>[value];
+  }
+
   @override
   Widget build(BuildContext context) {
     return PopScope(
@@ -29,7 +47,7 @@ class _AddNewStoryScreenState extends State<AddNewStoryScreen> {
       },
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Add New Story'),
+          title: const Text('Add New Story'),
         ),
         body: Center(
           child: ConstrainedBox(
@@ -40,18 +58,6 @@ class _AddNewStoryScreenState extends State<AddNewStoryScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  // TextFormField(
-                  //   controller: titleController,
-                  //   validator: (value) {
-                  //     if (value == null || value.isEmpty) {
-                  //       return 'Please enter your email.';
-                  //     }
-                  //     return null;
-                  //   },
-                  //   decoration: const InputDecoration(
-                  //     hintText: "Email",
-                  //   ),
-                  // ),
                   const SizedBox(height: 8),
                   TextFormField(
                     controller: descriptionController,
@@ -66,6 +72,105 @@ class _AddNewStoryScreenState extends State<AddNewStoryScreen> {
                       return null;
                     },
                   ),
+                  const SizedBox(height: 8),
+                  _previewImages(),
+                  const SizedBox(height: 8),
+                  ElevatedButton(
+                      onPressed: () async {
+                        await showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  "Confirm",
+                                ),
+                              ],
+                            ),
+                            content: const Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  "Silahkan pilih sumber gambar",
+                                  softWrap: true,
+                                  textAlign: TextAlign.justify,
+                                ),
+                              ],
+                            ),
+                            actions: [
+                              Container(
+                                margin: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Flexible(
+                                      child: Container(
+                                        height: 45,
+                                        width:
+                                            MediaQuery.of(context).size.width,
+                                        child: ElevatedButton(
+                                          onPressed: () async {
+                                            await _onImageButtonPressed(
+                                                ImageSource.gallery,
+                                                context: context);
+                                            Navigator.of(context).pop();
+                                          },
+                                          style: ElevatedButton.styleFrom(
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(8.0)),
+                                            backgroundColor: Colors.greenAccent,
+                                            side: const BorderSide(
+                                                color: Colors.greenAccent),
+                                          ),
+                                          child: const Text(
+                                            "Gallery",
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 16),
+                                    Flexible(
+                                      child: Container(
+                                        height: 45,
+                                        width:
+                                            MediaQuery.of(context).size.width,
+                                        child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(8.0)),
+                                            backgroundColor: Colors.greenAccent,
+                                            side: const BorderSide(
+                                                color: Colors.greenAccent),
+                                          ),
+                                          onPressed: () async {
+                                            await _onImageButtonPressed(
+                                                ImageSource.camera,
+                                                context: context);
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: const Text(
+                                            "Camera",
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                        // _onImageButtonPressed(ImageSource.gallery,
+                        //     context: context);
+                      },
+                      child: const Text("Pilih Gambar")),
                   const SizedBox(height: 8),
                   context.watch<AddStoryProvider>().isLoadingAddStory
                       ? const Center(child: CircularProgressIndicator())
@@ -93,4 +198,175 @@ class _AddNewStoryScreenState extends State<AddNewStoryScreen> {
       ),
     );
   }
+
+  Future<void> _onImageButtonPressed(
+    ImageSource source, {
+    required BuildContext context,
+    bool isMultiImage = false,
+    bool isMedia = false,
+  }) async {
+    try {
+      final XFile? pickedFile = await _picker.pickImage(
+        source: source,
+        // maxWidth: maxWidth,
+        // maxHeight: maxHeight,
+        // imageQuality: quality,
+      );
+      setState(() {
+        _setImageFileListFromFile(pickedFile);
+      });
+    } catch (e) {
+      setState(() {
+        _pickImageError = e;
+      });
+    }
+    // await _displayPickImageDialog(context, false,
+    //     (double? maxWidth, double? maxHeight, int? quality, int? limit) async {
+    //   try {
+    //     final XFile? pickedFile = await _picker.pickImage(
+    //       source: source,
+    //       maxWidth: maxWidth,
+    //       maxHeight: maxHeight,
+    //       imageQuality: quality,
+    //     );
+    //     setState(() {
+    //       _setImageFileListFromFile(pickedFile);
+    //     });
+    //   } catch (e) {
+    //     setState(() {
+    //       _pickImageError = e;
+    //     });
+    //   }
+    // });
+  }
+
+  Text? _getRetrieveErrorWidget() {
+    if (_retrieveDataError != null) {
+      final Text result = Text(_retrieveDataError!);
+      _retrieveDataError = null;
+      return result;
+    }
+    return null;
+  }
+
+  Widget _previewImages() {
+    final Text? retrieveError = _getRetrieveErrorWidget();
+    if (retrieveError != null) {
+      return retrieveError;
+    }
+    if (_mediaFileList != null) {
+      return Semantics(
+        label: 'image_picker_example_picked_images',
+        child: ListView.builder(
+          shrinkWrap: true,
+          physics: ClampingScrollPhysics(),
+          key: UniqueKey(),
+          itemBuilder: (BuildContext context, int index) {
+            return Semantics(
+              label: 'image_picker_example_picked_image',
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 300,
+                  width: 100,
+                  child: Image.file(
+                    File(_mediaFileList![index].path),
+                    errorBuilder: (BuildContext context, Object error,
+                        StackTrace? stackTrace) {
+                      return const Center(
+                          child: Text('This image type is not supported'));
+                    },
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            );
+          },
+          itemCount: _mediaFileList!.length,
+        ),
+      );
+    } else if (_pickImageError != null) {
+      return Text(
+        'Pick image error: $_pickImageError',
+        textAlign: TextAlign.center,
+      );
+    } else {
+      return const Text(
+        'You have not yet picked an image.',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+  Future<void> _displayPickImageDialog(
+      BuildContext context, bool isMulti, OnPickImageCallback onPick) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Add optional parameters'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextField(
+                  controller: maxWidthController,
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  decoration: const InputDecoration(
+                      hintText: 'Enter maxWidth if desired'),
+                ),
+                TextField(
+                  controller: maxHeightController,
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  decoration: const InputDecoration(
+                      hintText: 'Enter maxHeight if desired'),
+                ),
+                TextField(
+                  controller: qualityController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      hintText: 'Enter quality if desired'),
+                ),
+                if (isMulti)
+                  TextField(
+                    controller: limitController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                        hintText: 'Enter limit if desired'),
+                  ),
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                  child: const Text('PICK'),
+                  onPressed: () {
+                    final double? width = maxWidthController.text.isNotEmpty
+                        ? double.parse(maxWidthController.text)
+                        : null;
+                    final double? height = maxHeightController.text.isNotEmpty
+                        ? double.parse(maxHeightController.text)
+                        : null;
+                    final int? quality = qualityController.text.isNotEmpty
+                        ? int.parse(qualityController.text)
+                        : null;
+                    final int? limit = limitController.text.isNotEmpty
+                        ? int.parse(limitController.text)
+                        : null;
+                    onPick(width, height, quality, limit);
+                    Navigator.of(context).pop();
+                  }),
+            ],
+          );
+        });
+  }
 }
+
+typedef OnPickImageCallback = void Function(
+    double? maxWidth, double? maxHeight, int? quality, int? limit);
